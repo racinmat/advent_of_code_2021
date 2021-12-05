6fcef48ad15700f9cc07982cd4ebcefdff131610
Matěj Račinský <matej.racinsky@avast.com>
2021-12-05 17:55:19 +0100

    optimized day 5

--- day_05/main.jl
+++ day_05/main.jl
@@ -3,53 +3,67 @@ module Day05
 using DrWatson
 quickactivate(@__DIR__)
 using OffsetArrays
+# using TimerOutputs
+# using BenchmarkTools
 include(projectdir("misc.jl"))
+
+# const to = TimerOutput()
+# reset_timer!(to)
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
 # const raw_data = cur_day |> read_file("input_test.txt")
 
-parse_row(x) = split(x, " -> ") .|> (x->split(x, ",") .|> x->parse(Int, x)) |> x->reduce(hcat, x)
+parse_row(x) = split(x, " -> ") .|> (x->split(x, ",") .|> x->parse(Int32, x)) |> x->reduce(hcat, x)
 is_axis_aligned(x::Matrix) = x[1,1] == x[1,2] || x[2,1] == x[2,2]
 process_data() = raw_data |> read_lines .|> parse_row
 
-function get_points_between(i)
-    dif = i[:,2] - i[:,1]
-    max_dif = maximum(abs.(dif))
-    (i[:,1]+Int.(dif/max_dif)*j for j in 0:max_dif)
-    # [i[:,1]+Int.(dif/max_dif)*j for j in 0:max_dif]
+function assign2points_between!(grid, i)
+    #= @timeit to "dif" =# dif = i[:,2] - i[:,1]
+    #= @timeit to "max_dif" =# max_dif = maximum(abs, dif)
+    #= @timeit to "normed" =# normed_vec = dif .÷ max_dif
+    #= @timeit to "point_list" =# for j in 0:max_dif
+        k, l = i[:,1].+normed_vec*j
+        grid[k, l] += 1
+    end
 end
 
 # Analytic approach where I examine all unordered pairs (combinations of 2) and for intersecting lines I would
 # list points, get that list of points and then change it to Set to get unique number of points and count it
 # is doable, but because of thousands of points and relative small dimensions (1k x 1k) of the grid, the numeric approach is better.
 function part1()
-    data = process_data()
-    max_x = max(maximum(getindex.(data, 1)), maximum(getindex.(data, 3)))
-    min_x = min(minimum(getindex.(data, 1)), minimum(getindex.(data, 3)))
-    max_y = max(maximum(getindex.(data, 2)), maximum(getindex.(data, 4)))
-    min_y = min(minimum(getindex.(data, 2)), minimum(getindex.(data, 4)))
-    grid = OffsetArray(zeros(Int, 1-min_x+max_x, 1-min_y+max_y), (min_x:max_x, min_y:max_y))
-    axis_aligned = filter(is_axis_aligned, data)
-    for i in axis_aligned
-        for j in get_points_between(i)
-            grid[j...] += 1
-        end
+    #= @timeit to "process_data" =# data = process_data()
+    #= @timeit to "ext1" =# min1, max1 = extrema(x->getindex(x, 1), data)
+    #= @timeit to "ext2" =# min2, max2 = extrema(x->getindex(x, 2), data)
+    #= @timeit to "ext3" =# min3, max3 = extrema(x->getindex(x, 3), data)
+    #= @timeit to "ext4" =# min4, max4 = extrema(x->getindex(x, 4), data)
+    #= @timeit to "max_x" =# max_x = max(max1, max3)
+    #= @timeit to "min_x" =# min_x = min(min1, min3)
+    #= @timeit to "max_y" =# max_y = max(max2, max4)
+    #= @timeit to "min_y" =# min_y = min(min2, min4)
+    #= @timeit to "grid" =# grid = OffsetArray(zeros(Int8, 1-min_x+max_x, 1-min_y+max_y), (min_x:max_x, min_y:max_y))
+    #= @timeit to "axis_aligned" =# axis_aligned = filter(is_axis_aligned, data)
+    # i = axis_aligned[1]
+    
+    #= @timeit to "all_lines" =# for i in axis_aligned
+        assign2points_between!(grid, i)
     end
     sum(grid .> 1)
 end
 
 function part2()
     data = process_data()
-    max_x = max(maximum(getindex.(data, 1)), maximum(getindex.(data, 3)))
-    min_x = min(minimum(getindex.(data, 1)), minimum(getindex.(data, 3)))
-    max_y = max(maximum(getindex.(data, 2)), maximum(getindex.(data, 4)))
-    min_y = min(minimum(getindex.(data, 2)), minimum(getindex.(data, 4)))
-    grid = OffsetArray(zeros(Int, 1-min_x+max_x, 1-min_y+max_y), (min_x:max_x, min_y:max_y))
+    #= @timeit to "ext1" =# min1, max1 = extrema(x->getindex(x, 1), data)
+    #= @timeit to "ext2" =# min2, max2 = extrema(x->getindex(x, 2), data)
+    #= @timeit to "ext3" =# min3, max3 = extrema(x->getindex(x, 3), data)
+    #= @timeit to "ext4" =# min4, max4 = extrema(x->getindex(x, 4), data)
+    #= @timeit to "max_x" =# max_x = max(max1, max3)
+    #= @timeit to "min_x" =# min_x = min(min1, min3)
+    #= @timeit to "max_y" =# max_y = max(max2, max4)
+    #= @timeit to "min_y" =# min_y = min(min2, min4)
+    grid = OffsetArray(zeros(Int8, 1-min_x+max_x, 1-min_y+max_y), (min_x:max_x, min_y:max_y))
     for i in data
-        for j in get_points_between(i)
-            grid[j...] += 1
-        end
+        assign2points_between!(grid, i)
     end
     sum(grid .> 1)
 end
@@ -58,7 +72,10 @@ end
 end # module
 
 if false
+# Day05.reset_timer!(Day05.to)
 println(Day05.part1())
+# show(Day05.to)
+# @btime Day05.part1()
 Day05.submit(Day05.part1(), Day05.cur_day, 1)
 println(Day05.part2())
 Day05.submit(Day05.part2(), Day05.cur_day, 2)
