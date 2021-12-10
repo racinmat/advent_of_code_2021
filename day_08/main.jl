f433b7c8ddc021a3a92d9e95d4403aff72f8016c
Matěj Račinský <matej.racinsky@avast.com>
2021-12-11 00:36:05 +0100

    added test inputs, done day 8 and 9

--- day_08/main.jl
+++ day_08/main.jl
@@ -3,25 +3,88 @@ module Day08
 using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
+parse_row(x) = split(x, " | ") .|> x->split(x, " ")
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
-
+# const raw_data = cur_day |> read_file("input_test.txt")
+# const raw_data = cur_day |> read_file("input_test2.txt")
+process_data() = raw_data |> read_lines .|>parse_row
+# todo: you may 
 function part1()
     data = process_data()
+    outputs = getindex.(data, 2)
+    count.(x -> length(x) ∈ [2,3,4,7], outputs) |> sum
+end
+
+const seg2num = Dict(
+    "abcdef" => 0,
+    "bc" => 1,
+    "abdeg" => 2,
+    "abcdg" => 3,
+    "bcfg" => 4,
+    "acdfg" => 5,
+    "acdefg" => 6,
+    "abc" => 7,
+    "abcdefg" => 8,
+    "abcdfg" => 9,
+)
+num2seg = Dict(v=>k for (k,v) in seg2num)
+
+function solve_row(pattern, output)
+    in_numbers = @.(pattern |> collect |> sort |> join)
+    out_numbers = @.(output |> collect |> sort |> join)
+    mapping = Dict{Char,Union{Vector{Char},Nothing}}(i => collect("abcdefg") for i in collect("abcdefg"))
+    # searching for simple numbers
+    for i in [1,4,7,8]
+        comb = filter(x->length(x)==length(num2seg[i]), in_numbers) |> first
+        for j in collect(comb)
+            mapping[j] = intersect(mapping[j], collect(num2seg[i]))
+        end
+    end
+    solution, _ = solve_options(mapping) do sol
+        all(x->join(sort(getindex.(Ref(sol), collect(x)))) ∈ keys(seg2num), in_numbers)
+    end
+    nums = map(x->seg2num[join(sort(getindex.(Ref(solution), collect(x))))], out_numbers)
+    reduce((a,b)->a*10+b, nums)
+end
+
+function solve_options(is_valid, mapping)
+    if all(kv->length(kv[2]) == 1, mapping)
+        return Dict(k=>v[1] for (k, v) in mapping), length(unique(values(mapping))) == length(mapping)
+    end
+    a_letter, options = filter(kv->length(kv[2])>1, mapping) |> first
+    @inbounds for i in 1:length(options)
+        mapping_try = copy(mapping)
+        mapping_try[a_letter] = options[i:i]
+        solution_valid = true
+        @inbounds for k in setdiff(keys(mapping_try), Set(a_letter))
+            mapping_try[k] = setdiff(mapping_try[k], mapping_try[a_letter])
+            if isempty(mapping_try[k])
+                solution_valid = false
+            end
+        end
+        !solution_valid && continue
+        res, was_success = solve_options(is_valid, mapping_try)
+        was_success && is_valid(res) && return res, was_success
+    end
+    mapping, false
 end
 
 function part2()
     data = process_data()
+    sum(solve_row(pattern, output) for (pattern, output) in data)
 end
 
 
 end # module
 
 if false
+using BenchmarkTools
 println(Day08.part1())
+@btime Day08.part1()
 Day08.submit(Day08.part1(), Day08.cur_day, 1)
 println(Day08.part2())
+@btime Day08.part2()
 Day08.submit(Day08.part2(), Day08.cur_day, 2)
 end
--- day_09/main.jl
+++ day_09/main.jl
@@ -2,18 +2,69 @@ module Day09
 
 using DrWatson
 quickactivate(@__DIR__)
+using ImageFiltering, OffsetArrays, StatsBase
 include(projectdir("misc.jl"))
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+# const raw_data = cur_day |> read_file("input_test.txt")
+process_data() = (raw_data |> read_lines .|> x->split(x, "") .|> x->parse.(Int16, x)) |> x->reduce(hcat, x)'
+
+function find_lowpoints(data)
+    k1 = OffsetArray([
+        -1 1
+    ], 0:0, 0:1)
+    k2 = OffsetArray([
+        1 -1
+    ], 0:0, -1:0)
+    k3 = OffsetArray([
+        -1 1
+    ]', 0:1, 0:0)
+    k4 = OffsetArray([
+        1 -1
+    ]', -1:0, -0:0)
+    lowpoints = 
+        (imfilter(data, k4, "reflect") .> 0) .+ 
+        (imfilter(data, k3, "reflect") .> 0) .+ 
+        (imfilter(data, k2, "reflect") .> 0) .+ 
+        (imfilter(data, k1, "reflect") .> 0) .== 4
+    lowpoints
+end
+
+function get_in_4neighborhood(m, i, cond)
+    neighbors = CartesianIndex[]
+    @inbounds i.I[1] > 1 && push!(neighbors, CartesianIndex(-1, 0))
+    @inbounds i.I[2] > 1 && push!(neighbors, CartesianIndex(0, -1))
+    @inbounds i.I[1] < size(m)[1] && push!(neighbors, CartesianIndex(1, 0))
+    @inbounds i.I[2] < size(m)[2] && push!(neighbors, CartesianIndex(0, 1))
+    @inbounds [j for j in neighbors if cond(i+j)]
+end
 
 function part1()
     data = process_data()
+    lowpoints = find_lowpoints(data)
+    sum(data[lowpoints]) + sum(lowpoints)
 end
 
 function part2()
     data = process_data()
+    lowpoints = find_lowpoints(data)
+    basins = zeros(UInt8, size(data))
+    basins[lowpoints] = 1:sum(lowpoints)
+    points2visit = findall(>(0), lowpoints)
+    while !isempty(points2visit)
+        # p = points2visit[1]
+        points2visit_next = []
+        for p in points2visit
+            for q in get_in_4neighborhood(basins, p, x->basins[x] == 0 && data[x] != 9)
+                r = q+p
+                basins[r] = basins[p]
+                push!(points2visit_next, r)
+            end
+        end
+        points2visit = points2visit_next
+    end
+    prod(partialsort(counts(basins)[2:end], 1:3, rev=true))
 end
 
 
