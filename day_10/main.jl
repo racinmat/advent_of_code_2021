94b378c7a4910c6a7cbdcee504d92e212aaaf078
Matěj Račinský <matej.racinsky@avast.com>
2021-12-13 01:33:12 +0100

    done day 10, day 11

--- day_10/main.jl
+++ day_10/main.jl
@@ -2,18 +2,83 @@ module Day10
 
 using DrWatson
 quickactivate(@__DIR__)
+using DataStructures, Statistics
 include(projectdir("misc.jl"))
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+process_data() = raw_data |> read_lines
+
+const closes = Dict(
+    '>' => '<',
+    ']' => '[',
+    '}' => '{',
+    ')' => '(',
+)
+const part1points = Dict(
+    '>' => 25137,
+    ']' => 57,
+    '}' => 1197,
+    ')' => 3,
+)
+const part2points = Dict(
+    '<' => 4,
+    '[' => 2,
+    '{' => 3,
+    '(' => 1,
+)
+
+function calc_row_part1(row)
+    s = Stack{Char}()
+    ch = row[5]
+    ch = ')'
+    for ch in row
+        if ch ∈ "(<[{"
+            push!(s, ch)
+        else
+            if first(s) == closes[ch]
+                pop!(s)
+            else
+                return part1points[ch]
+            end
+        end
+    end
+    return 0
+end
+
+function calc_row_part2(row)
+    total_score = 0
+    s = Stack{Char}()
+    ch = row[5]
+    ch = ')'
+    for ch in row
+        if ch ∈ "(<[{"
+            push!(s, ch)
+        else
+            if first(s) == closes[ch]
+                pop!(s)
+            else
+                return part1points[ch]
+            end
+        end
+    end
+    while !isempty(s)
+        ch = pop!(s)
+        total_score *= 5
+        total_score += part2points[ch]
+    end
+    return total_score
+end
 
 function part1()
     data = process_data()
+    mapreduce(calc_row_part1, +, data)
 end
 
 function part2()
     data = process_data()
+    incomplete_rows = filter(x -> calc_row_part1(x) == 0, data)
+    Int(median(calc_row_part2.(incomplete_rows)))
 end
 
 
--- day_11/main.jl
+++ day_11/main.jl
@@ -6,14 +6,63 @@ include(projectdir("misc.jl"))
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+# const raw_data = cur_day |> read_file("input_test.txt")
+process_data() = (raw_data |> read_lines .|> x->split(x, "") .|> x->parse.(Int16, x)) |> x->reduce(hcat, x)'
+
+function get_in_8neighborhood(m, i, cond)
+    neighbors = CartesianIndex[]
+    @inbounds i.I[1] > 1 && i.I[2] > 1 && push!(neighbors, CartesianIndex(-1, -1))
+    @inbounds i.I[1] > 1 && push!(neighbors, CartesianIndex(-1, 0))
+    @inbounds i.I[1] > 1 && i.I[2] < size(m)[2] && push!(neighbors, CartesianIndex(-1, 1))
+    @inbounds i.I[2] > 1 && push!(neighbors, CartesianIndex(0, -1))
+    @inbounds i.I[1] < size(m)[1] && i.I[2] > 1 && push!(neighbors, CartesianIndex(1, -1))
+    @inbounds i.I[1] < size(m)[1] && push!(neighbors, CartesianIndex(1, 0))
+    @inbounds i.I[2] < size(m)[2] && push!(neighbors, CartesianIndex(0, 1))
+    @inbounds i.I[1] < size(m)[1] && i.I[2] < size(m)[2] && push!(neighbors, CartesianIndex(1, 1))
+    @inbounds [j for j in neighbors if cond(i+j)]
+end
+
+function iteration(data)
+    new_data = data .+ 1
+    flashes = findall(new_data .> 9)
+    new_data[flashes] .= 0
+    num_flashes = length(flashes)
+    for f in flashes
+        for j in get_in_8neighborhood(new_data, f, (i)->true)
+            k = j+f
+            if k ∉ flashes
+                new_data[k] += 1
+            end
+            if new_data[k] > 9
+                push!(flashes, k)
+                num_flashes += 1
+                new_data[k] = 0
+            end
+        end
+    end
+    new_data, num_flashes
+end
 
 function part1()
     data = process_data()
+    sum_flashes = 0
+    for i in 1:100
+        data, new_flashes = iteration(data)
+        sum_flashes += new_flashes
+    end
+    sum_flashes
 end
 
 function part2()
     data = process_data()
+    length(data)
+    for i in 1:10_000
+        data, new_flashes = iteration(data)
+        if new_flashes == length(data)
+            return i
+        end
+    end
+    return 0
 end
 
 
