d0f5ae4fa7aedda62f65e549abd269834f99419f
Matěj Račinský <matej.racinsky@avast.com>
2021-12-21 14:55:43 +0100

    day 21 part 2 done

--- day_21/main.jl
+++ day_21/main.jl
@@ -2,7 +2,7 @@ module Day21
 
 using DrWatson
 quickactivate(@__DIR__)
-using Base.Iterators
+using Base.Iterators, StatsBase
 include(projectdir("misc.jl"))
 
 function parse_row(row)
@@ -11,8 +11,8 @@ function parse_row(row)
 end
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
-# const raw_data = cur_day |> read_input
-const raw_data = cur_day |> read_file("input_test.txt")
+const raw_data = cur_day |> read_input
+# const raw_data = cur_day |> read_file("input_test.txt")
 process_data() = raw_data |> read_lines .|> parse_row |> Dict
 
 incr_pos_mod(a, b, base=10) = (a + b - 1) % base + 1
@@ -40,34 +40,38 @@ function part1()
     minimum(values(scores)) * num_rolls
 end
 
-const all_die_options = reshape(product(1:3, 1:3, 1:3) |> collect, :)
+# const all_die_options = reshape(product(1:3, 1:3, 1:3) |> collect, :)
+const all_die_options = reshape(product(1:3, 1:3, 1:3) |> collect, :) .|> sum |> countmap
+
 function play_game(positions, scores, player1)
-    # max_score = 21
-    max_score = 6
-    i = all_die_options |> first
+    max_score = 21
+    # max_score = 10
+    # i = all_die_options |> first
     wins1, wins2 = 0, 0
-    for i in all_die_options
+    # if same sum has occured more times, I can count it once and multiply win nums
+    for (i_sum, num_occurs) in all_die_options
         positions2 = copy(positions)
         scores2 = copy(scores)
         p_idx = player1 ? 1 : 2
-        positions2[p_idx] = incr_pos_mod(positions2[p_idx], sum(i))
+        positions2[p_idx] = incr_pos_mod(positions2[p_idx], i_sum)
         scores2[p_idx] += positions2[p_idx]
         if scores2 |> values |> maximum >= max_score
             if scores2[1] >= max_score
-                wins1 += 1
+                wins1 += num_occurs
             else
-                wins2 += 1
+                wins2 += num_occurs
             end
         else
             wins1_i, wins2_i = play_game(positions2, scores2, !player1)
-            wins1 += wins1_i
-            wins2 += wins2_i
+            wins1 += wins1_i * num_occurs
+            wins2 += wins2_i * num_occurs
         end
     end
     wins1, wins2
 end
 
 function part2()
+    # instead of the naive iteration, I can use histogram
     positions = process_data()
     # this takes too long, does not work
     scores = Dict(k=>0 for k in keys(positions))
@@ -76,7 +80,8 @@ function part2()
     wins1, wins2 = play_game(positions, scores, player1)
     max(wins1, wins2)
 end
-
+# for original implementation and max_score 6
+# (30498, 7203)
 
 end # module
 
@@ -86,5 +91,6 @@ println(Day21.part1())
 @btime Day21.part1()
 Day21.submit(Day21.part1(), Day21.cur_day, 1)
 println(Day21.part2())
+@btime Day21.part2()
 Day21.submit(Day21.part2(), Day21.cur_day, 2)
 end
