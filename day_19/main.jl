7cda83f7a510f630e915cfb005d58667a5f9aa63
Matěj Račinský <matej.racinsky@avast.com>
2021-12-21 11:07:00 +0100

    day 21 part 1 done

--- day_19/main.jl
+++ day_19/main.jl
@@ -4,11 +4,11 @@ using DrWatson
 quickactivate(@__DIR__)
 using Combinatorics, Rotations, Base.Iterators, Distances, DataStructures
 using Pipe:@pipe
-using TimerOutputs, BenchmarkTools
+# using TimerOutputs, BenchmarkTools
 include(projectdir("misc.jl"))
 
-const to = TimerOutput()
-reset_timer!(to)
+# const to = TimerOutput()
+# reset_timer!(to)
 
 parse_point(row) = @pipe row |> split(_, ",") |> parse.(Int, _)
 function parse_scanner(scanner_lines)
@@ -35,27 +35,28 @@ const all_bases = map(reshape(product(angles,angles,angles) |> collect, :)) do (
 end |> unique
 
 function beacons2dists(beacons, rel_base = nothing)
-    @timeit to "sortrows_beacon" beacons_aligned = sortrows(hcat(isnothing(rel_base) ? beacons : beacons * rel_base, 1:size(beacons,1)))
-    @timeit to "pairwise" dists = pairwise(dist, @view(beacons_aligned[:,1:3]), dims=1)
+    #= @timeit to "sortrows_beacon" =# beacons_aligned = sortrows(hcat(isnothing(rel_base) ? beacons : beacons * rel_base, 1:size(beacons,1)))
+    #= @timeit to "pairwise" =# dists = pairwise(dist, @view(beacons_aligned[:,1:3]), dims=1)
     dists, beacons_aligned
 end
 
 function beacon_pairs2dists(beacons1, beacons2, b2key, rel_base, precomputed_dists)
     # I must compute beacons1 again every time, because the number of beacons is rising
-    @timeit to "dists_beacon1" dists1, beacons1_aligned = beacons2dists(beacons1)
+    #= @timeit to "dists_beacon1" =# dists1, beacons1_aligned = beacons2dists(beacons1)
     # beacons2 are precomputed
     # @timeit to "dists_beacon2" dists2, beacons2_aligned = beacons2dists(beacons2, rel_base)
-    @timeit to "dists_beacon2" dists2, beacons2_aligned = precomputed_dists[b2key][rel_base]
+    #= @timeit to "dists_beacon2" =# dists2, beacons2_aligned = precomputed_dists[b2key][rel_base]
     dists1, dists2, beacons1_aligned, beacons2_aligned
 end
 
 function get_viable_bases(beacons1_base, beacons2_base, b2key, precomputed_dists)
     viable_bases = Matrix{Int64}[]
-    @timeit to "dists_beacon1" dists1, _ = beacons2dists(beacons1_base)
+    #= @timeit to "dists_beacon1" =# dists1, _ = beacons2dists(beacons1_base)
+    #= @timeit to "dists1set" =# dists1set = Set(dists1)
     for (i, base) in enumerate(all_bases)
-        @timeit to "dists_beacon2" dists2, _ = precomputed_dists[b2key][rel_base]
+        #= @timeit to "dists_beacon2" =# dists2, _ = precomputed_dists[b2key][base]
         # if only 1 point intersects, we have no intersecting points
-        @timeit to "intersect_all_dists" length(intersect(dists1, dists2)) <= 2 && continue
+        #= @timeit to "intersect_all_dists" =# length(intersect(dists1set, dists2)) <= 2 && continue
         push!(viable_bases, base)
     end
     viable_bases
@@ -66,7 +67,7 @@ function match_pairs_in_dists(dists1, dists2)
     dists1rows = [Set(i) for i in eachrow(dists1)]
     dists2rows = [Set(i) for i in eachrow(dists2)]
     for (i,j) in product(1:size(dists1,1),1:size(dists2,2))
-        @timeit to "intersect_rows" length(intersect(dists1rows[i], dists2rows[j])) < 12 && continue
+        #= @timeit to "intersect_rows" =# length(intersect(dists1rows[i], dists2rows[j])) < 12 && continue
         push!(same_pairs, [i,j])
     end
     same_pairs
@@ -74,12 +75,12 @@ end
 
 # beacons1_base, beacons2_base = beacon_positions, data[i]
 function find_viable_base(beacons1_base, beacons2_base, b2key, precomputed_dists)
-    @timeit to "get_viable_bases" viable_bases = get_viable_bases(beacons1_base, beacons2_base, b2key, precomputed_dists)
+    #= @timeit to "get_viable_bases" =# viable_bases = get_viable_bases(beacons1_base, beacons2_base, b2key, precomputed_dists)
     # base = viable_bases[1]
-    @timeit to "dists_beacon1" dists1, beacons1 = beacons2dists(beacons1_base)
+    #= @timeit to "dists_beacon1" =# dists1, beacons1 = beacons2dists(beacons1_base)
     for base in viable_bases
-        @timeit to "dists_beacon2" dists2, beacons2 = precomputed_dists[b2key][rel_base]
-        @timeit to "match_pairs_in_dists" same_pairs = match_pairs_in_dists(dists1, dists2)
+        #= @timeit to "dists_beacon2" =# dists2, beacons2 = precomputed_dists[b2key][base]
+        #= @timeit to "match_pairs_in_dists" =# same_pairs = match_pairs_in_dists(dists1, dists2)
         length(same_pairs) < 12 && continue    
         # init with first scanner results, then add the rest
         beacon1_rows = getindex.(same_pairs, 1)
@@ -107,7 +108,7 @@ function process_scanners(f, unprocessed_scanners, beacon_positions, data, preco
     while !isempty(unprocessed_scanners)
         i = dequeue!(unprocessed_scanners)
         # @info "examining scanner" i
-        @timeit to "find_viable_base" trans_vec, base = find_viable_base(beacon_positions, data[i], i, precomputed_dists)
+        #= @timeit to "find_viable_base" =# trans_vec, base = find_viable_base(beacon_positions, data[i], i, precomputed_dists)
         if isnothing(trans_vec) && isnothing(base)
             enqueue!(unprocessed_scanners, i)
             continue
@@ -125,7 +126,7 @@ precompute_rel_base_dists(data) = Dict(k=>Dict(b=>beacons2dists(beacons, b) for 
 function part1()
     data = process_data()
     beacon_positions = data[0]
-    @timeit to "precompute_rel_base_dists" precomputed_dists = precompute_rel_base_dists(data)
+    #= @timeit to "precompute_rel_base_dists" =# precomputed_dists = precompute_rel_base_dists(data)
     unprocessed_scanners = keys2unprocessed_scanners(keys(data))
     beacon_positions = process_scanners(unprocessed_scanners, beacon_positions, data, precomputed_dists) do merged_beacons, _
         merged_beacons
@@ -136,9 +137,10 @@ end
 function part2()
     data = process_data()
     beacon_positions = data[0]
+    #= @timeit to "precompute_rel_base_dists" =# precomputed_dists = precompute_rel_base_dists(data)
     unprocessed_scanners = keys2unprocessed_scanners(keys(data))
     scanner_positions = [[0,0,0]]
-    beacon_positions = process_scanners(unprocessed_scanners, beacon_positions, data) do merged_beacons, trans_vec
+    beacon_positions = process_scanners(unprocessed_scanners, beacon_positions, data, precomputed_dists) do merged_beacons, trans_vec
         push!(scanner_positions, trans_vec[1,:])
         merged_beacons
     end
@@ -158,31 +160,3 @@ Day19.submit(Day19.part1(), Day19.cur_day, 1)
 println(Day19.part2())
 Day19.submit(Day19.part2(), Day19.cur_day, 2)
 end
-
-#  ──────────────────────────────────────────────────────────────────────────────────
-#                                            Time                   Allocations
-#                                    ──────────────────────   ───────────────────────
-#          Tot / % measured:              4.62s / 100%            2.73GiB / 100%
-
-#  Section                   ncalls     time   %tot     avg     alloc   %tot      avg
-#  ──────────────────────────────────────────────────────────────────────────────────
-#  find_viable_base              85    4.62s   100%  54.3ms   2.72GiB  100%   32.8MiB
-#    get_viable_bases            85    3.78s  81.7%  44.4ms   2.16GiB  79.4%  26.1MiB
-#      intersect_all_dists    2.04k    3.31s  71.6%  1.62ms   1.51GiB  55.6%   779KiB
-#      beacon_pairs2dists     2.04k    465ms  10.1%   228μs    663MiB  23.8%   333KiB
-#        dists_beacon1        2.04k    440ms  9.52%   216μs    641MiB  23.0%   322KiB
-#          pairwise           2.04k    388ms  8.41%   190μs    599MiB  21.5%   301KiB
-#          sortrows_beacon    2.04k   48.9ms  1.06%  24.0μs   42.1MiB  1.51%  21.2KiB
-#        dists_beacon2        2.04k   22.6ms  0.49%  11.1μs   22.1MiB  0.79%  11.1KiB
-#          sortrows_beacon    2.04k   12.8ms  0.28%  6.26μs   9.70MiB  0.35%  4.87KiB
-#          pairwise           2.04k   7.98ms  0.17%  3.91μs   12.3MiB  0.44%  6.19KiB
-#    match_pairs_in_dists       141    804ms  17.4%  5.70ms    508MiB  18.2%  3.60MiB
-#      intersect_rows          742k    546ms  11.8%   736ns    284MiB  10.2%     401B
-#    beacon_pairs2dists         141   38.2ms  0.83%   271μs   66.7MiB  2.39%   484KiB
-#      dists_beacon1            141   36.1ms  0.78%   256μs   65.2MiB  2.34%   473KiB
-#        pairwise               141   31.5ms  0.68%   223μs   61.5MiB  2.20%   446KiB
-#        sortrows_beacon        141   4.39ms  0.09%  31.1μs   3.68MiB  0.13%  26.8KiB
-#      dists_beacon2            141   1.79ms  0.04%  12.7μs   1.53MiB  0.05%  11.1KiB
-#        sortrows_beacon        141   1.03ms  0.02%  7.30μs    688KiB  0.02%  4.88KiB
-#        pairwise               141    600μs  0.01%  4.26μs    876KiB  0.03%  6.21KiB
-#  ──────────────────────────────────────────────────────────────────────────────────
--- day_21/main.jl
+++ day_21/main.jl
@@ -2,25 +2,87 @@ module Day21
 
 using DrWatson
 quickactivate(@__DIR__)
+using Base.Iterators
 include(projectdir("misc.jl"))
 
+function parse_row(row)
+    m = match(r"Player (\d+) starting position: (\d+)", row)
+    parse(Int16, m[1]) => parse(Int16, m[2])
+end
+
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
-const raw_data = cur_day |> read_input
-process_data() = raw_data
+# const raw_data = cur_day |> read_input
+const raw_data = cur_day |> read_file("input_test.txt")
+process_data() = raw_data |> read_lines .|> parse_row |> Dict
+
+incr_pos_mod(a, b, base=10) = (a + b - 1) % base + 1
 
 function part1()
-    data = process_data()
+    positions = process_data()
+    scores = Dict(k=>0 for k in keys(positions))
+    dice = 1:100
+    player1 = true
+    # i = partition(cycle(dice), 3) |> first
+    num_rolls = 0
+    for i in partition(cycle(dice), 3)
+        num_rolls += 3
+        p_idx = player1 ? 1 : 2
+        positions[p_idx] = incr_pos_mod(positions[p_idx], sum(i))
+        scores[p_idx] += positions[p_idx]
+        player1 = !player1
+        if scores |> values |> maximum >= 1000
+            # @info "break" i
+            break
+        end
+    end
+    # positions
+    # scores
+    minimum(values(scores)) * num_rolls
+end
+
+const all_die_options = reshape(product(1:3, 1:3, 1:3) |> collect, :)
+function play_game(positions, scores, player1)
+    max_score = 21
+    i = all_die_options |> first
+    wins1, wins2 = 0, 0
+    for i in all_die_options
+        positions2 = copy(positions)
+        scores2 = copy(scores)
+        p_idx = player1 ? 1 : 2
+        positions2[p_idx] = incr_pos_mod(positions2[p_idx], sum(i))
+        scores2[p_idx] += positions2[p_idx]
+        if scores2 |> values |> maximum >= max_score
+            if scores2[1] >= max_score
+                wins1 += 1
+            else
+                wins2 += 1
+            end
+        else
+            wins1_i, wins2_i = play_game(positions2, scores2, !player1)
+            wins1 += wins1_i
+            wins2 += wins2_i
+        end
+    end
+    wins1, wins2
 end
 
 function part2()
-    data = process_data()
+    positions = process_data()
+    # this takes too long, does not work
+    # scores = Dict(k=>0 for k in keys(positions))
+    # wins1, wins2 = 0, 0
+    # player1 = true
+    # wins1, wins2 = play_game(positions, scores, player1)
+    # max(wins1, wins2)
 end
 
 
 end # module
 
 if false
+using BenchmarkTools
 println(Day21.part1())
+@btime Day21.part1()
 Day21.submit(Day21.part1(), Day21.cur_day, 1)
 println(Day21.part2())
 Day21.submit(Day21.part2(), Day21.cur_day, 2)
